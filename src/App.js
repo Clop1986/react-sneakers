f228fa16954b515b2c2534ee32449c89b537fbb3
Дмитрий Гринь <dzmitry.hryn@gmail.com>
2021-08-27 12:05:09 +0300

    output product cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Drawer from './components/Drawer';
 import Header from './components/Header';
 import Card from './components/Card';
 
+const arr = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imageUrl: '/img/sneakers/1.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 15600,
+    imageUrl: '/img/sneakers/2.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imageUrl: '/img/sneakers/3.jpg'
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imageUrl: '/img/sneakers/4.jpg'
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">      
@@ -15,8 +38,14 @@ function App() {
             <input type="text" placeholder="Поиск ..." />
           </div>
         </div>
-        <div className="d-flex">          
-          <Card />
+        <div className="d-flex">                 
+          {arr.map((obj) => (
+            <Card 
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+            />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/Card/index.js
@@ -0,0 +1,25 @@
+import styles from './Card.module.scss'
+
+function Card(props) {
+  return (
+
+    <div className={styles.card}>
+      <div className={styles.favorite}>
+        <img src="/img/heart-unliked.svg" alt="Unliked" />
+      </div>
+      <img width="133" height="122" src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{props.price} руб.</b>
+        </div>
+        <button className="button">
+          <img width="11" height="11" src="/img/plus.svg" alt="Button" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
